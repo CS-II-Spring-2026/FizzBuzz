0ebdb2ec41a7d2e52fd1ead13d2872891d78101a
kaster2029-sys <kaster2029@ecsdcards.com>
2026-02-04 08:17:22 -0600

    Rylan's file

---
+++ RK-Fizzbuzz.py
@@ -0,0 +1,23 @@
+# for i in range(1, 11, 1):
+#      print(i)
+
+# x = 0
+# for i in range(1,11):
+#     x=x + 2
+#     print (x)
+# x = 11
+# for i in range(1,11):
+#     x-=1
+#     print (x)
+# print ("blast off🚀")
+
+##Rylan Kaster
+for i in range(1,101):
+    if i % 3 == 0 and i % 5 == 0:
+            print ("fizzbuzz")
+    elif i % 3 == 0:
+          print("fizz")
+    elif i % 5 == 0:
+          print("buzz")
+    else:
+          print(i)
