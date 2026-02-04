3026783ce6f5e0d33c58a321e05070108b38aac0
novak2027-png <novak2027@ecsdcards.com>
2026-02-04 08:13:16 -0600

    Add files via upload

---
+++ Fizzbuzz.py
@@ -0,0 +1,31 @@
+#modulo
+
+
+# for i in range(1,11):
+#     if i % 2 ==0:
+#         print(i, " is Even")
+#     else:
+#         print(i, " is Odd")
+
+
+# x=1
+# for i in range (1,11):
+#     x=x+2
+#     print(x)
+
+# for i in range (1,11):
+#     # x=x-1
+#     x-=1
+#     print(x)
+# print("Blast off!!")
+#Jakub Novak
+for i in range(1,101):
+    if i % 3==0 and i % 5==0:
+        print("Fizzbuzz")
+    elif i % 3==0:
+        print("Fizz")
+    elif i % 5==0:
+        print("Buzz")
+    else:
+        print(i)
+    
